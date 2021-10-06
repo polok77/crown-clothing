b7a462aa0f3e1fea1e203bf1a8c305edd7936491
Ahsan Ullah <ahsan.ullah@ericsson.com>
2021-10-07 00:01:28 +0600

    integratibg with firebase and signin with google account

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+const config = {
+    apiKey: "AIzaSyAh1QSvEAhDqvZnOo_zuFuRrTK8y-cDhkg",
+    authDomain: "crown-eco.firebaseapp.com",
+    projectId: "crown-eco",
+    storageBucket: "crown-eco.appspot.com",
+    messagingSenderId: "477805299972",
+    appId: "1:477805299972:web:955e398d2b1724bdeaa851",
+    measurementId: "G-0PZWRJHD10"
+  };
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
